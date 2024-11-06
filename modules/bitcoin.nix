52726b1d4633d8c600a6b2bf87a75181fc202b05
willcl-ark <will@256k1.dev>
2024-11-06 21:52:44 +0000

    add signet node

--- modules/bitcoin.nix
+++ modules/bitcoin.nix
@@ -1,6 +1,24 @@
 { config, lib, pkgs, ... }:
 {
-  services.bitcoind.node = {
+  services.bitcoind.signet = {
+    enable = true;
+    dbCache = 16000;
+    extraCmdlineOptions = [ "-signet" ];
+
+    # Benchmark nodes can be whitelisted by connecting to the whitebind port.
+    # Note: this could be changed to `whitelist` of ip addresses if abused
+    #
+    # Add the following option to extraConfig to disable outbound connections
+    # noconnect=1
+    extraConfig = ''
+      [signet]
+      listen=1
+      server=1
+      whitebind=download,noban@0.0.0.0:55555
+    '';
+  };
+
+  services.bitcoind.mainnet = {
     enable = true;
     dbCache = 16000;
 
@@ -12,9 +30,9 @@
     extraConfig = ''
       listen=1
       server=1
-      whitebind=download,noban@0.0.0.0:55555
+      whitebind=download,noban@0.0.0.0:33333
     '';
   };
 
-  networking.firewall.allowedTCPPorts = [ 8333 8332 55555 ];
+  networking.firewall.allowedTCPPorts = [ 8333 33333 38333 55555 ];
 }
