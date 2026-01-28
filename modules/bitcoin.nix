23d95174696d2a61b62ae7b6f216fff65f61ff54
will <will@256k1.dev>
2026-01-28 14:30:17 +0000

    bitcoin: add whitelisted ips

--- modules/bitcoin.nix
+++ modules/bitcoin.nix
@@ -1,8 +1,26 @@
 { ... }:
 let
-  whitelisted_ips = [ "148.251.128.115" "65.21.224.151" ];
+  whitelisted_ips = [ "148.251.128.115" "65.21.224.151" "203.161.35.209" "150.228.36.148" ];
   bitcoin_ports = [ 8333 33333 38333 55555 ];
 in {
+  services.bitcoind.mainnet = {
+    enable = true;
+    dbCache = 16000;
+
+    # Benchmark nodes can be whitelisted by connecting to the whitebind port.
+    # Note: this could be changed to `whitelist` of ip addresses if abused
+    #
+    # Add the following option to extraConfig to disable outbound connections
+    # noconnect=1
+    extraConfig = ''
+      listen=1
+      noconnect=1
+      server=1
+      whitebind=download,noban@0.0.0.0:8333
+      whitebind=download,noban@0.0.0.0:33333
+    '';
+  };
+
   services.bitcoind.signet = {
     enable = true;
     dbCache = 16000;
@@ -20,24 +38,6 @@ in {
       server=1
       whitebind=download,noban@0.0.0.0:38333
       whitebind=download,noban@0.0.0.0:55555
-    '';
-  };
-
-  services.bitcoind.mainnet = {
-    enable = true;
-    dbCache = 16000;
-
-    # Benchmark nodes can be whitelisted by connecting to the whitebind port.
-    # Note: this could be changed to `whitelist` of ip addresses if abused
-    #
-    # Add the following option to extraConfig to disable outbound connections
-    # noconnect=1
-    extraConfig = ''
-      listen=1
-      noconnect=1
-      server=1
-      whitebind=download,noban@0.0.0.0:8333
-      whitebind=download,noban@0.0.0.0:33333
     '';
   };
 
